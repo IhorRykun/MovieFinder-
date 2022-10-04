d2ac8a073d488817de1e65451839749a387b1b20
Ihor <ihorrykun@gmail.com>
2022-10-04 22:35:47 +0300

    відкоригували стиль сайту та зробили запити

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import NewApiMovieService from './apiServer';
+import renderMovieMarkup from './renderMovie';
 
 const movieGallery = new NewApiMovieService();
 
 const form = document.querySelector('.form__search');
+const movie = document.querySelector('.render__movie');
 
 form.addEventListener('submit', onSubmitForm);
 
 function onSubmitForm(e) {
-    e.preventDefault();
-    movieGallery.query = e.currentTarget.elements.search.value;
-    if (movieGallery.query === "") {
-        alert('Вибачте, ведіть назву фільму');
-    }
-     movieGallery.fethcMovie();
+  e.preventDefault();
+  movieGallery.query = e.currentTarget.elements.search.value;
+  if (movieGallery.query === '') {
+    alert('Введіть назву фільму');
+  }
+  fethcMovie();
+  movie.innerHTML = '';
 }
 
+async function fethcMovie() {
+  const response = await movieGallery.fethcMovie();
+  const { Search } = response;
+}
--- src/renderMovie.js
+++ src/renderMovie.js
@@ -1,11 +1,15 @@
-const movie = document.querySelector('.render__movie');
-
 export { renderMovieMarkup };
+const movie = document.querySelector('.render__movie');
 
 function renderMovieMarkup(movie) {
     const markup = movie.map(mov => {
+        console.log(mov);
         const { Poster, Title, Year } = mov;
-
-        return `<div class=""></div>`;
-    });
+        return `<div class="container__movie">
+        <img src="${Poster}"></img>
+        <p>${Title}</p>
+        <p>${Year}</p>
+        </div>`;
+    }).join('');
+  movie.insertAdjacentHTML('beforeend', markup);
 }
