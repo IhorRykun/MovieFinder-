ec57e3b12c434dade26363caf0d453fe83280bae
Ihor <ihorrykun@gmail.com>
2022-10-08 19:34:55 +0300

    aa

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const movieGallery = new NewApiMovieService();
 
 const form = document.querySelector('.form__search');
 const movie = document.querySelector('.render__movie');
+const containerScroll = document.querySelector('.container__scroll');
 
 form.addEventListener('submit', onSubmitForm);
 
@@ -16,12 +17,30 @@ function onSubmitForm(e) {
     }
         fethcMovie();
     movie.innerHTML = '';
-  
+    registerIntersetObserv();
 }
 
 async function fethcMovie() {
     const response = await movieGallery.fethcMovie();
     const search = response.Search;
     await renderMovieMarkup(search);
-    
+}
+
+
+function registerIntersetObserv() {
+    const onEntry = entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting && movieGallery.query !== '') {
+              movieGallery.incrementPage();
+              fethcMovie();
+            }
+        });
+    };
+    const options = {
+      rootMargin: '1000px',
+    };
+
+
+    const observe = new IntersectionObserver(onEntry, options);
+    observe.observe(containerScroll);
 }
